eee90831abd10fe6a011c31807faa60bb0930e91
Mario Solomou <webmaster@masodev.com>
2014-09-07 16:26:03 +0300

    Variables

---
+++ Variables/name.rb
@@ -0,0 +1,9 @@
+puts "Please type your First name:"
+name = gets.chomp
+
+puts "Please type your Last name:"
+last_name = gets.chomp
+
+puts "Welcome #{name +" "+ last_name}!"
+
+10.times { puts name }
