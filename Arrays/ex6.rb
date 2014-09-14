37a560d70b280d30915879629ae7f92b24257289
Mario Solomou <webmaster@masodev.com>
2014-09-14 22:40:19 +0300

    Arrays

---
+++ Arrays/ex6.rb
@@ -0,0 +1,4 @@
+names = ['bob', 'joe', 'susan', 'margaret']
+names[3] = 'jody'
+
+#Arrays items can be accessed by the index which is an integer
---
+++ Arrays/ex7.rb
@@ -0,0 +1,9 @@
+arr = [1,2,3,4]
+arr1 = []
+
+arr.each do |num|
+  arr1.push(num + 2)
+end
+
+p arr
+p arr1
